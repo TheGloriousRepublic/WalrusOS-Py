7e44bb521e959041f5a0619e322f97411ef49694
hppavilion1 <nathantronnes@gmail.com>
2015-05-26 11:31:31 -0800

    Started on Parser

--- WaSh_parser.py
+++ WaSh_parser.py
@@ -1,1 +1,19 @@
+from WaSh_AST import *
+from WaSh_lexer import *
 from WaSh_parser_combinators import *
+
+RESERVED = 'RESERVED'
+STRING = 'STRING'
+INT = 'INT'
+DEC = 'DEC'
+NAME = 'NAME'
+
+def keyword(kw):
+    return Reserved(kw, RESERVED)
+
+name = Tag(NAME)
+integer = Tag(INT) ^ (lambda i: int(i))
+dec = Tag(DEC) ^ (lambda i: float(i))
+
+def aexp_value():
+    return (num ^ (lambda i: IntExp(i))) | (id  ^ (lambda v: VarExp(v)))
