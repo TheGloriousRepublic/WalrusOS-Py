ea97ab48cecb62c9b56bd46aef404c9627669c70
hppavilion1 <nathantronnes@gmail.com>
2015-05-26 11:40:22 -0800

    Fixed WaSh_AST

--- WaSh_AST.py
+++ WaSh_AST.py
@@ -8,112 +8,112 @@ class Equality: #Test if two things are the same
 #Arithmetical Expressions
 class IntExp(Equality): #Integer
 	def __init__(self, i):
-		self.i=i
+	    self.i=i
 	
 	def __repr__(self):
-		return('IntExp('+self.i+')')
+	    return('IntExp('+self.i+')')
 
 class StrExp(Equality): #String Expressions
 	def __init__(self, s):
-		self.s = s
+	    self.s = s
 	
 	def __repr__(self):
-		return('Exp('+self.s+')')
+	    return('Exp('+self.s+')')
 	
 class VarExp(Equality): #Variable
 	def __init__(self, name):
-		self.name = name
+	    self.name = name
 	
 	def __repr__(self):
-		return('VarExp('+self.name+')')
+	    return('VarExp('+self.name+')')
 	
 class BiExp(Equality): #Binary expression (e.g. 2+2)
-	def __init__(self, op, l, r)
-		self.op = op
-		self.left = l
-		self.right = r
+	def __init__(self, op, l, r):
+	    self.op = op
+            self.left = l
+            self.right = r
 	
 	def __repr__(self):
-		return('BiExp('+self.op+', '+self.left+', '+self.right+')')
+	    return('BiExp('+self.op+', '+self.left+', '+self.right+')')
 
 #Boolean Expressions
 class RBExp(Equality):
-	def __init__(self, b):
-		self.b = b
+    def __init__(self, b):
+        self.b = b
 	
-	def __repr__(self):
-		return('RBExp('+self.b+')')
+    def __repr__(self):
+        return('RBExp('+self.b+')')
 
 class RelopBExp(Equality):
-	def __init__(self, op, l, r)
-		self.op = op
-		self.left = l
-		self.right = r
-		
-	def __repr__(self):
-		return('BiExp('+self.op+', '+self.left+', '+self.right+')')
+    def __init__(self, op, l, r):
+        self.op = op
+        self.left = l
+        self.right = r
+    
+    def __repr__(self):
+        return('BiExp('+self.op+', '+self.left+', '+self.right+')')
 
 class AndBExp(Equality):
     def __init__(self, l, r):
         self.left = l
-		self.right = r
+        self.right = r
 
-	def __repr__(self):
-		return('AndBExp('+self.left+', '+self.right+')')
+    def __repr__(self):
+        return('AndBExp('+self.left+', '+self.right+')')
 	
-class OrBExp(Bexp):
+class OrBExp(Equality):
     def __init__(self, l, r):
         self.left = l
-		self.right = r
+        self.right = r
 	
-	def __repr__(self):
-		return('OrBExp('+self.left+', '+self.right+')')
-		
-class XorBExp(Bexp):
+    def __repr__(self):
+        return('OrBExp('+self.left+', '+self.right+')')
+    
+class XorBExp(Equality):
     def __init__(self, l, r):
         self.left = l
-		self.right = r
+        self.right = r
 	
-	def __repr__(self):
-		return('XorBExp('+self.left+', '+self.right+')')
+    def __repr__(self):
+        return('XorBExp('+self.left+', '+self.right+')')
 
-class NotBExp(Bexp):
+class NotBExp(Equality):
     def __init__(self, e):
         self.exp = e
-		
-	def __repr__(self):
-		return('NotBExp('+self.exp+')')
-		
+    
+    def __repr__(self):
+        return('NotBExp('+self.exp+')')
+    
 #Statements
 class AssignStatement(Equality):
     def __init__(self, name, e):
          self.name = name
-		 self.exp = e
-		 
-	def __repr__(self):
-		return('AssignStatement('+self.name+', '+self.e+')')
+         self.exp = e
+     
+    def __repr__(self):
+        return('AssignStatement('+self.name+', '+self.e+')')
 
 class CompoundStatement(Equality):
     def __init__(self, first, second):
         self.first = first
-		self.second = second
-		
-	def __repr__(self):
-		return('CompoundStatement('+self.first+', '+self.second+')')
+        self.second = second
+    
+    def __repr__(self):
+        return('CompoundStatement('+self.first+', '+self.second+')')
 	
 class IfStatement(Equality):
     def __init__(self, condition, true_stmt, false_stmt):
         self.condition = condition
-		self.ontrue = true_stmt
-		self.onfalse = false_stmt
-		
-	def __repr__(self):
-		return('IfStatement('+condition+', '+self.ontrue+', '+self.onfalse+')')
+        self.ontrue = true_stmt
+        self.onfalse = false_stmt
+
+    def __repr__(self):
+        return('IfStatement('+condition+', '+self.ontrue+', '+self.onfalse+')')
 
 class WhileStatement(Equality):
     def __init__(self, condition, body):
         self.condition = condition
-		self.body = body
+        self.body = body
 	
-	def __repr__(self):
-		return('WhileStatement('+self.condition+', '+self.body+')')
+    def __repr__(self):
+        return('WhileStatement('+self.condition+', '+self.body+')')
--- WaSh_parser.py
+++ WaSh_parser.py
@@ -2,12 +2,6 @@ from WaSh_AST import *
 from WaSh_lexer import *
 from WaSh_parser_combinators import *
 
-RESERVED = 'RESERVED'
-STRING = 'STRING'
-INT = 'INT'
-DEC = 'DEC'
-NAME = 'NAME'
-
 def keyword(kw):
     return Reserved(kw, RESERVED)
 
@@ -16,4 +10,4 @@ integer = Tag(INT) ^ (lambda i: int(i))
 dec = Tag(DEC) ^ (lambda i: float(i))
 
 def aexp_value():
-    return (num ^ (lambda i: IntExp(i))) | (id  ^ (lambda v: VarExp(v)))
+    return (integer ^ (lambda i: IntExp(i))) | (dec ^ (lambda i: IntExp(i))) | (id  ^ (lambda v: VarExp(v)))
